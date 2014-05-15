e68d40f610d6d49549ff80aba1e1b307dc5ed942
111884 <mpyymann@gmail.com>
2014-05-15 14:29:28 +0000

    Users can edit and update their profiles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]
+  before_action :correct_user,   only: [:edit, :update]
   def show
     @user = User.find(params[:id])
   end
@@ -7,7 +9,7 @@ class UsersController < ApplicationController
 	@user = User.new
   end
   
-def create
+  def create
     @user = User.new(user_params)
     if @user.save
 	  sign_in @user
@@ -17,6 +19,18 @@ def create
       render 'new'
     end
   end
+  
+  def edit
+  end	
+  
+  def update
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
 
   private
 
@@ -24,4 +38,19 @@ def create
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
+	
+	# Before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
+    end
+
 end
